0370f6dacefc70beaa5458e61aeae61c41485b2e
jakeschlaerth <jakeschlaerth1999@gmail.com>
2022-07-10 20:56:32 -0400

    removed references to slave, paramaterized db configs in init files

---
+++ db_init/master_init/master_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "*** creating replica user and default db... ***"
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "
+    CREATE USER '$REPLICA_USER'@'%' IDENTIFIED BY '$REPLICA_PASSWORD';
+    GRANT REPLICATION SLAVE, SELECT ON *.* TO '$REPLICA_USER'@'%';
+    CREATE DATABASE primary_db;"
+
+echo "*** finished creating replica user and default db ***"
--- db_init/master_init/master_init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'repluser'@'%' IDENTIFIED BY 'replsecret';
-GRANT REPLICATION SLAVE ON *.* TO 'repluser'@'%';
-CREATE DATABASE primary_db;
